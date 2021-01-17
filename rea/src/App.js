9898325fc7202d8b38774ab81e785596d53f3017
Rafail Dardagiannopoulos <77059808+Rafail1992@users.noreply.github.com>
2021-01-17 16:07:10 +0200

    Version V1.6

---
+++ rea/src/App.js
@@ -0,0 +1,13 @@
+import Form from '../src/Form';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
